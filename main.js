e5e9ad54fe480b3820f66115eebce8ea5aa8833c
526cloiacono <526cloiacono@frhsd.com>
2025-11-25 11:49:32 -0500

    mid way through lesson 5

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+        product: 'Socks',
+        description: 'A warm fuzzy pair of socks',
+        image: './assets/images/socks_green.jpg',
+        inventory: 11 , 
+        details: ['80% cotton', '20% polyester',' 30% spandex']
+    }
+}
+  })
